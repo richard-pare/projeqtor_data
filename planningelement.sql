52c9587dac8dfc16054d956322a9b472bee9e12d
Richard Paré <rick.pare69@gmail.com>
2017-09-03 12:54:17 -0400

    Update planningelement.sql

--- planningelement.sql
+++ planningelement.sql
@@ -1,4 +1,6 @@
-select project.name as "Projet",assignedWork as "Efforts prévus",realWork as "Efforts effectués",leftwork as "Efforts restants",plannedwork as "Efforts réévalués", assignedCost as "Coûts prévus", realCost as "Coûts réels",leftCost as "Coûts restants", plannedCost as "Coûts réévalués"
+select project.name as "Projet",type.name as "Type projet",assignedWork as "Efforts prévus",realWork as "Efforts effectués",leftwork as "Efforts restants",plannedwork as "Efforts réévalués", assignedCost as "Coûts prévus", realCost as "Coûts réels",leftCost as "Coûts restants", plannedCost as "Coûts réévalués"
 from planningelement
-join project ON planningelement.idProject = project.id
+left join project ON planningelement.idProject = project.id
+left join type ON project.idProjectType = type.id
 where planningelement.refType = "Project"
+and planningelement.idle = 0
