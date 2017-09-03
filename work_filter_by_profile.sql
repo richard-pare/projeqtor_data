b475a4aca73fef6be84dabe3548feb022b29ca7b
Richard Paré <rick.pare69@gmail.com>
2017-09-03 19:07:19 -0400

    Create work_filter_by_profile.sql

---
+++ work_filter_by_profile.sql
@@ -0,0 +1,9 @@
+select project.name as "Nom projet",activity.name as "Nom activité", resource.fullName as "Nom ressource",role.name as "Fonction", work.workDate as "Journée",work.work as "Effort",work.cost as "Coût"
+from work 
+join project ON work.idProject = project.id
+join activity ON (work.refId = activity.id and work.refType = "Activity")
+join resource on work.idResource = resource.id
+left join assignment on work.idAssignment = assignment.id
+left join role on assignment.idRole = role.id
+left join profile on resource.idProfile = profile.id
+where profile.profileCode = "ETM"
