877664e448e00d8a90bb48ccf641f5547f5236de
Richard Paré <rick.pare69@gmail.com>
2017-09-03 18:13:30 -0400

    Update work_group_by_week.sql

--- work_group_by_week.sql
+++ work_group_by_week.sql
@@ -1,4 +1,4 @@
-select project.name as "Project name",activity.name as "Activity name", resource.fullName as "Resource name",role.name as "Function", work,week,sum(work.work) as "Sum of work",sum(work.cost) as "Sum of cost"
+select project.name as "Nom projet",activity.name as "Nom activité", resource.fullName as "Nom ressource",role.name as "Fonction", work.week as "Semaine",sum(work.work) as "Sum of work",sum(work.cost) as "Sum of cost"
 from work 
 join project ON work.idProject = project.id
 join activity ON (work.refId = activity.id and work.refType = "Activity")
