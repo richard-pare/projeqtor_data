e31fcfef18eb1bc26b4d30ffad7d92d18a5be05f
Richard Paré <rick.pare69@gmail.com>
2017-09-04 13:03:13 -0400

    Delete work_group_by_year.sql

--- work_group_by_year.sql
+++
@@ -1,8 +0,0 @@
-select project.name as "Nom projet",activity.name as "Nom activité", resource.fullName as "Nom ressource",role.name as "Fonction", work.year as "Année",sum(work.work) as "Sum of work",sum(work.cost) as "Sum of cost"
-from work 
-join project ON work.idProject = project.id
-join activity ON (work.refId = activity.id and work.refType = "Activity")
-join resource on work.idResource = resource.id
-left join assignment on work.idAssignment = assignment.id
-left join role on assignment.idRole = role.id
-group by resource.fullName,role.name,work.year
